6b6d0541e29115e77bcb7db2662a061dc0e10733
Hetarth Salat <99407331+Skullcrusher161@users.noreply.github.com>
2026-05-19 11:43:36 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,11 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   output: "server",
   adapter: vercel({
-    webAnalytics: { enabled: false },
     imageService: false,
   }),
   integrations: [
     tailwind({ applyBaseStyles: false }),
   ],
-  image: {
-    service: {
-      entrypoint: "astro/assets/services/noop",
-    },
-  },
   vite: {
     ssr: {
       external: ["sharp", "sanity"],
